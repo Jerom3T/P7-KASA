3f516924d809106d758d8538e18a0830a5edc1a7
Jerom3T <jerom3tonnelier@gmail.com>
2023-05-21 17:56:41 +0200

    Premier Commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './sass/index.scss';
+import './sass/components/navigation.scss';
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
