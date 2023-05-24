d8371cd61919490ccfeecdf42cc4b915dbce02a9
Jerom3T <jerom3tonnelier@gmail.com>
2023-05-24 18:36:23 +0200

    Maj Images

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
+
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Appart from "./pages/Appart";
+import NotFound from "./pages/NotFound";
 
 const App = () => {
   return (
@@ -10,10 +12,8 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
-        <Route path="/details/:id" element={<Appart />} />      
-
-
-
+        <Route path="/details/:id" element={<Appart />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
   );
--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,12 +1,12 @@
 import React from 'react';
-
+import logo from '../images/logo.png';
 
 const Logo = () => {
-    return (
-        <div className="logo">
-            <img src="./logo.png" alt="logo" title='Kasa' />
-        </div>
-    );
+  return (
+    <div className="logo">
+      <img src={logo} alt="logo" title="Kasa" />
+    </div>
+  );
 };
 
 export default Logo;
